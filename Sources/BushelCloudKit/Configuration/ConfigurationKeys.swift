337474474af0ba1862de9dc78dced6b61ef97fd2
Leo Dion <leogdion@brightdigit.com>
2025-12-30 09:27:36 -0500

    fixup! Migrate from ArgumentParser to Swift Configuration (#13)

--- Sources/BushelCloudKit/Configuration/ConfigurationKeys.swift
+++ Sources/BushelCloudKit/Configuration/ConfigurationKeys.swift
@@ -2,7 +2,29 @@
 //  ConfigurationKeys.swift
 //  BushelCloud
 //
-//  Configuration keys for reading from providers
+//  Created by Leo Dion.
+//  Copyright © 2025 BrightDigit.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 //
 
 import ConfigKeyKit
@@ -67,7 +89,8 @@ internal enum ConfigurationKeys {
   /// Sync command configuration keys (using base key with BUSHEL prefix)
   internal enum Sync {
     internal static let dryRun = ConfigKey<Bool>(bushelPrefixed: "sync.dry_run")
-    internal static let restoreImagesOnly = ConfigKey<Bool>(bushelPrefixed: "sync.restore_images_only")
+    internal static let restoreImagesOnly = ConfigKey<Bool>(
+      bushelPrefixed: "sync.restore_images_only")
     internal static let xcodeOnly = ConfigKey<Bool>(bushelPrefixed: "sync.xcode_only")
     internal static let swiftOnly = ConfigKey<Bool>(bushelPrefixed: "sync.swift_only")
     internal static let noBetas = ConfigKey<Bool>(bushelPrefixed: "sync.no_betas")
